6d362b29512e3541f989394b06015c612a838e2b
Onur <onuryuksektepeli@hotmail.com.tr>
2020-09-06 00:45:13 +0300

    Initial commit

---
+++ Assets/Scripts/ActivateObject.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActivateObject : MonoBehaviour
+{
+    public GameObject myObject;
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameObject.SetActive(false);
+        //Debug.Log("Active Self: " + myObject.activeSelf);
+        //Debug.Log("Active in Hierarchy" + myObject.activeInHierarchy);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
